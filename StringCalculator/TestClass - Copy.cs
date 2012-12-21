2850a877f02ef969ac6a5d9fc35550c75ce2bc40
oleksiyo <kozzzak@gmail.com>
2012-12-21 14:44:04 +0200

    1

---
+++ StringCalculator/TestClass - Copy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace StringCalculator
+{
+    public class TestClass2
+    {
+        [Fact]
+        public void FailedTest1()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void FailedTest2()
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
