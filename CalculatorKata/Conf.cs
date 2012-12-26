9512507deb3eed81193afdd4336fd94647f360a8
unknown <oleksiyo@mak019.compatibl.com>
2012-12-26 13:56:52 +0200

    Test12

---
+++ CalculatorKata/Conf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata
+{
+  public static class Conf
+    {
+      public static string start = "//";
+      public static string end = "\n";
+      public static string dStart = "[";
+      public static string dEnd = "]";
+      public static string exceptionMsg = "Negatives not allowed:";
+    }
+}
